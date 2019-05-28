ef564cd98555e201514b57b4dcd2997226cdf59d
leiyanfei <630519552@qq.com>
2019-05-28 11:07:31 +0800

    11:07

--- src/pages/login/login.vue
+++ src/pages/login/login.vue
@@ -1,24 +1,34 @@
 <template>
     <div class="home">
         <yd-layout>
-            <yd-navbar slot="navbar" title="NavBar">
+            <yd-navbar slot="navbar" title="登录">
                 <router-link to="#" slot="left">
                     <yd-navbar-back-icon></yd-navbar-back-icon>
                 </router-link>
             </yd-navbar>
 
-            <div>乱七八糟的内容在这里，超出容器，将出现滚动条。</div>
-            <div>乱七八糟的内容在这里，超出容器，将出现滚动条。</div>
-            <div>乱七八糟的内容在这里，超出容器，将出现滚动条。</div>
-
+            <yd-cell-group>
+                <yd-cell-item>
+                    <span slot="left">用户名：</span>
+                    <input slot="right" type="number" placeholder="请输入用户名">
+                </yd-cell-item>
+                <yd-cell-item>
+                    <span slot="left">密&nbsp;&nbsp;&nbsp;&nbsp;码：</span>
+                    <input slot="right" type="number" placeholder="请输入手密码">
+                </yd-cell-item>
+            </yd-cell-group>
+            
+            <div class="login_btn_box">
+                <yd-button size="large" type="primary">登入</yd-button>
+            </div>
             <yd-tabbar slot="tabbar">
-                <yd-tabbar-item title="首页" link="#" active>
+                <yd-tabbar-item title="首页" link="#" >
                     <yd-icon name="home" slot="icon"></yd-icon>
                 </yd-tabbar-item>
                 <yd-tabbar-item title="购物车" link="#">
                     <yd-icon name="shopcart-outline" slot="icon"></yd-icon>
                 </yd-tabbar-item>
-                <yd-tabbar-item title="个人中心" link="#">
+                <yd-tabbar-item title="个人中心" link="#" active>
                     <yd-icon name="ucenter-outline" slot="icon"></yd-icon>
                 </yd-tabbar-item>
             </yd-tabbar>
@@ -35,9 +45,12 @@ export default {
     } 
 }
 </script>
-<style scoped>
+<style scoped lang="scss">
 .home{
     height: 100%;
+    .login_btn_box{
+        margin: 0.5rem;
+    }
 }
 </style>
 
